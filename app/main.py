a0a1281bfa431717bc7c35f5bf8e9ee048465625
Oleg Feshchenko <oleg.feshchenko@hmmr.ru>
2026-05-05 11:37:59 +0300

    main

--- app/main.py
+++ app/main.py
@@ -43,15 +43,12 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,  # ⚠️ важно
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"],
 )
+
 # Настраиваем логгер при старте приложения
 logger = setup_logger(log_level="DEBUG")  # или "INFO" для прода
 
